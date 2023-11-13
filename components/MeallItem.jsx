5157a44020ffda62d64bd5c227b08ba1687a6c87
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-13 18:58:52 +0100

    Filtering and displaying on the MEALS array

---
+++ components/MeallItem.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MeallItem = ({title}) => {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  )
+}
+
+export default MeallItem
+
+const styles = StyleSheet.create({})
--- screens/MealsOvervieawScreen.jsx
+++ screens/MealsOvervieawScreen.jsx
@@ -1,22 +1,34 @@
-import { View, Text,StyleSheet } from 'react-native'
-import React from 'react'
-import {MEALS} from '../data/dummy-data'
+import { View, StyleSheet, FlatList } from "react-native";
+import React from "react";
+import { MEALS } from "../data/dummy-data";
+import MeallItem from "../components/MeallItem";
 
-const MealsOvervieawScreen = ( {route}) => {    
-    const catId = route.params.categoryId
-    
+const MealsOvervieawScreen = ({ route }) => {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MeallItem title={itemData.item.title} />;
+  }
   return (
     <View style={styles.constainer}>
-      <Text>MealsOvervieawScreen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default MealsOvervieawScreen
+export default MealsOvervieawScreen;
 
-const styles =StyleSheet.create({
-    constainer:{
-        flex:1,
-        padding:16
-    }
-})
+const styles = StyleSheet.create({
+  constainer: {
+    flex: 1,
+    padding: 16,
+  },
+});
