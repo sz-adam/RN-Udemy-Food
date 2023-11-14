6214cb50e6eb5f392ab86641667714769112a963
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-14 18:24:32 +0100

    subtitle componens addition

--- components/MealDetail/Subtitle.jsx
+++ components/MealDetail/Subtitle.jsx
@@ -1,14 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Subtitle = () => {
+const Subtitle = ({children}) => {
   return (
-    <View>
-      <Text>Subtitle</Text>
-    </View>
+    <View style={styles.subtitleContainer}>
+        <Text style={styles.subtitle}>{children}</Text>
+      </View>
   )
 }
 
 export default Subtitle
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    subtitle: {
+        color: '#e2b497',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+      subtitleContainer: {
+        margin: 6,
+        padding: 6,
+        marginHorizontal: 24,
+        marginVertical: 6,
+        textAlign: 'center',
+        borderBottomColor: '#e2b497',
+        borderBottomWidth: 2
+      }
+})
--- screens/MealDetailScreen.jsx
+++ screens/MealDetailScreen.jsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 import { MEALS } from '../data/dummy-data'
 import MealDetails from '../components/MealDetails'
+import Subtitle from '../components/MealDetail/Subtitle'
 
 const MealDetailScreen = ({ route }) => {
   const mealId = route.params.mealId
@@ -10,20 +11,20 @@ const MealDetailScreen = ({ route }) => {
 
   return (
     <View>
-      <Image style={styles.image} source={{uri: selectedMeal.imageUrl}}/>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
       <Text style={styles.title}>{selectedMeal.title}</Text>
-     
-        <MealDetails 
-        duration={selectedMeal.duration} 
-        complexity={selectedMeal.complexity} 
+
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
         affordability={selectedMeal.affordability}
         textStyle={styles.detailText}
-        />
-      
-      <Text>Ingredients</Text>
-      {selectedMeal.ingredients.map((ingredient) =><Text key={ingredient}>{ingredient}</Text>)}
-      <Text>Steps</Text>
-      {selectedMeal.steps.map((step) =><Text key={step}>{step}</Text>)}
+      />
+      <Subtitle>Ingredients</Subtitle>
+      {selectedMeal.ingredients.map((ingredient) => <Text key={ingredient}>{ingredient}</Text>)}
+
+      <Subtitle>Steps</Subtitle>
+      {selectedMeal.steps.map((step) => <Text key={step}>{step}</Text>)}
     </View>
   )
 }
@@ -31,18 +32,19 @@ const MealDetailScreen = ({ route }) => {
 export default MealDetailScreen
 
 const styles = StyleSheet.create({
-  image:{
-    width:'100%',
-    height:350,
+  image: {
+    width: '100%',
+    height: 350,
   },
-  title:{
-    fontWeight:'bold',
-    fontSize:24,
-    margin:8,
-    textAlign:'center',
-    color:'white',
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    margin: 8,
+    textAlign: 'center',
+    color: 'white',
   },
-  detailText:{
-    color:'white'
+  detailText: {
+    color: 'white'
   },
+
 })
