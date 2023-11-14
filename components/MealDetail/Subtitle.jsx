0eb8818bde0354c38361fed4b4010fc84105812e
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-14 18:21:20 +0100

    creation of 2 components for outsourcing

---
+++ components/MealDetail/Subtitle.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Subtitle = () => {
+  return (
+    <View>
+      <Text>Subtitle</Text>
+    </View>
+  )
+}
+
+export default Subtitle
+
+const styles = StyleSheet.create({})
