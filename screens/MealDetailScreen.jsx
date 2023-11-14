329809cab231334fe0074b9849757b34fa45331c
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-14 17:24:18 +0100

    add: mealDetailScreen

---
+++ screens/MealDetailScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MealDetailScreen = () => {
+  return (
+    <View>
+      <Text>MealDetailScreen</Text>
+    </View>
+  )
+}
+
+export default MealDetailScreen
+
+const styles = StyleSheet.create({})
