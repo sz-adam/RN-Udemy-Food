ce5c6ec8dc1127f5fc3828be094493e63a571969
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-16 20:14:09 +0100

    if there is no favorite text display

--- screens/FavoritesScreens.jsx
+++ screens/FavoritesScreens.jsx
@@ -1,16 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 import React, { useContext } from 'react'
 import MealList from '../components/MealList/MealList'
-import {FavoritesContext} from '../store/context/Favorites-context'
+import { FavoritesContext } from '../store/context/Favorites-context'
 import { MEALS } from "../data/dummy-data";
 
 const FavoritesScreens = () => {
   const favoriteMealsCtx = useContext(FavoritesContext);
   const favoriteMeals = MEALS.filter((meal) => favoriteMealsCtx.ids.includes(meal.id)
   )
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    )
+  }
   return <MealList items={favoriteMeals} />
 }
 
 export default FavoritesScreens
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white'
+  }
+})
